d244328e1a33571102dee93e4f8f793542cc550e
HN721 <119390246+HN721@users.noreply.github.com>
2025-02-21 07:03:57 +0700

    cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const io = socketio(server);
 //middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://whatsapp-six-pink.vercel.app"],
+    origin: "https://whatsapp-six-pink.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
